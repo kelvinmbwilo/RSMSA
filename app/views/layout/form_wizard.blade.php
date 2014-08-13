0409d2d09112f4f3e04269b0f20ec212397f2c1a
issyzac <issyzac.iz@gmail.com>
2014-08-13 22:49:41 +0300

    specifying stakeholders interface

--- app/views/layout/form_wizard.blade.php
+++ app/views/layout/form_wizard.blade.php
@@ -1,9 +1,21 @@
 <section class="panel">
     <header class="panel-heading">
-        Basic Forms
+        Add New Stakeholder
     </header>
     <div class="panel-body">
         <form role="form">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Name</label>
+                <input type="email" class="form-control" id="Sumatra" placeholder="Enter name">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Phone Number</label>
+                <input type="email" class="form-control" id="+255 712 223 234" placeholder="Enter phone number">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Address</label>
+                <input type="email" class="form-control" id="P O Box 2003 Arusha" placeholder="Enter address">
+            </div>
             <div class="form-group">
                 <label for="exampleInputEmail1">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
@@ -11,11 +23,6 @@
             <div class="form-group">
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-            </div>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox"> Check me out
-                </label>
             </div>
             <button type="submit" class="btn btn-info">Submit</button>
         </form>
--- app/views/stakeholders.blade.php
+++ app/views/stakeholders.blade.php
@@ -27,6 +27,7 @@
                                 <th class="numeric">Phone</th>
                                 <th class="numeric">Created</th>
                                 <th class="numeric">Updated</th>
+                                <th>Action</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -37,6 +38,16 @@
                                 <td class="numeric">{{$stakeHolder->phoneNumber}}</td>
                                 <td class="numeric">{{$stakeHolder->created_at}}</td>
                                 <td class="numeric">{{$stakeHolder->updated_at}}</td>
+                                <td class="table-condensed">
+                                    <div class="btn-group pull-right" >
+                                        <button type="submit" class="btn btn-primary"  data-toggle="modal" data-target="#myModal" value="Edit">
+                                            <i class="fa fa-edit"></i>
+                                        </button>
+                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target=".bs-example-modal-sm" >
+                                            <i class="fa fa-trash-o"></i>
+                                        </button>
+                                    </div>
+                                </td>
                             </tr>
                             @endforeach
                             </tbody>
@@ -50,22 +61,34 @@
     <!-- page end-->
 </section>
 
-<!-- Modal -->
+<!-- New Stakeholder Modal -->
 
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                <h4 class="modal-title" id="myModalLabel">Stakeholder</h4>
             </div>
             <div class="modal-body">
+
                 @include('layout.form_wizard')
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
                 <button type="button" class="btn btn-primary">Save changes</button>
             </div>
+        </div>
+    </div>
+</div>
+
+<!-- Delete modal -->
+
+<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            ...
         </div>
     </div>
 </div>
